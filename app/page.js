d9ecc81f61fe1023ead5772c7a43221b3b85420d
m-shao <s201083816@ddsbstudent.ca>
2023-07-29 20:45:16 -0400

    homepage reinstated

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
 import Public from '@components/components/Public';
 
 export default function Home() {
-
-    //uncomment Public to see the login button
     return (
-        // <Public />   
-        <>
-            
-        </>   
+        <Public />    
     )
 }
