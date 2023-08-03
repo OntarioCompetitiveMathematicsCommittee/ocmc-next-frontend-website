2ada79b9b453e9906165e33be8777f8477f2bdf8
yanziguoo <1guoyan@hdsb.ca>
2023-08-03 19:44:59 -0400

    comment participants layout file

--- app/portal/participants/layout.js
+++ app/portal/participants/layout.js
@@ -1,7 +1,9 @@
+// import required modules and components
 import RequireAuth from '@components/RequireAuth'
 import { ROLES } from '@config/roles'
 
 const ParticipantsLayout = ({ children }) => {
+    // restrict access to users with participant role
     return (
         <RequireAuth allowedRoles={[ROLES.Participant]}>
             {children}
