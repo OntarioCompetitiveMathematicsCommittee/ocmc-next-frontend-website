d7c67e6c427566192964e6a495a84820e7b46304
yanziguoo <1guoyan@hdsb.ca>
2023-08-03 14:15:55 -0400

    comment new contest page

--- app/portal/contests/new/page.js
+++ app/portal/contests/new/page.js
@@ -1,97 +1,109 @@
-"use client"
+"use client";
 
+// importing required modules and components
 import { useEffect, useState, useRef } from "react";
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 import { useAddNewContestMutation } from "@components/features/contests/contestsApiSlice";
 
 const NewContest = () => {
-    const errRef = useRef(null);
+  // reference to error message element
+  const errRef = useRef(null);
 
-    const [addNewContest, {
-        isLoading,
-        isSuccess,
-        isError,
-        error
-    }] = useAddNewContestMutation();
+  // mutation function for adding a new contest
+  const [addNewContest, { isLoading, isSuccess, isError, error }] = useAddNewContestMutation();
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const [name, setName] = useState('');
-    const [year, setYear] = useState('');
-    const [description, setDescription] = useState('');
-    const [max_score, setMaxScore] = useState('');
+  // state variables for contest details
+  const [name, setName] = useState("");
+  const [year, setYear] = useState("");
+  const [description, setDescription] = useState("");
+  const [max_score, setMaxScore] = useState("");
 
-    const handleNameChange = (e) => setName(e.target.value);
-    const handleYearChange = (e) => setYear(e.target.value);
-    const handleDescriptionChange = (e) => setDescription(e.target.value);
-    const handleMaxScoreChange = (e) => setMaxScore(e.target.value);
+  // event handlers that update state variables when input fields change
+  const handleNameChange = (e) => setName(e.target.value);
+  const handleYearChange = (e) => setYear(e.target.value);
+  const handleDescriptionChange = (e) => setDescription(e.target.value);
+  const handleMaxScoreChange = (e) => setMaxScore(e.target.value);
 
-    const canSubmit = [name, year, description, max_score].every(Boolean) && !isLoading;
+  // form can be submitted if all fields have values and are not in loading state
+  const canSubmit = [name, year, description, max_score].every(Boolean) && !isLoading;
 
-    useEffect(() => {
-        if (isSuccess) router.push('/portal/contests');
-    }, [isSuccess, router]);
+  // redirects to contest page after successful contest creation
+  useEffect(() => {
+    if (isSuccess) router.push("/portal/contests");
+  }, [isSuccess, router]);
 
-    const onCreateContestClicked = async (e) => {
-        e.preventDefault();
-        if (canSubmit) await addNewContest({ name, year, description, max_score });
-    }
+  // handles form submission
+  const onCreateContestClicked = async (e) => {
+    e.preventDefault();
+    if (canSubmit) await addNewContest({ name, year, description, max_score });
+  };
 
-    let errmsg;
-    if (isError) errmsg = error.error;
+  // error message handling
+  let errmsg;
+  if (isError) errmsg = error.error;
 
-    const content = (
-        <section>
-            <p ref={errRef} className={isError ? "errmsg" : "offscreen"} aria-live="assertive">{errmsg}</p>
+  // styling
+  const content = (
+    <section>
+      {/* display error message if there is an error */}
+      <p ref={errRef} className={isError ? "errmsg" : "offscreen"} aria-live="assertive">{errmsg}</p>
 
-            <h1>New Contest</h1>
+      <h1>New Contest</h1>
 
-            <form onSubmit={onCreateContestClicked}>
-                <label htmlFor="name">Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={name}
-                    onChange={handleNameChange}
-                />
+      <form onSubmit={onCreateContestClicked}>
+        {/** input field for contest name */}
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={name}
+          onChange={handleNameChange}
+        />
 
-                <label htmlFor="year">Year:</label>
-                <input
-                    type="text"
-                    id="year"
-                    name="year"
-                    value={year}
-                    onChange={handleYearChange}
-                />
+        {/** input field for contest year */}
+        <label htmlFor="year">Year:</label>
+        <input
+          type="text"
+          id="year"
+          name="year"
+          value={year}
+          onChange={handleYearChange}
+        />
 
-                <label htmlFor="description">Description:</label>
-                <textarea
-                    id="description"
-                    name="description"
-                    value={description}
-                    onChange={handleDescriptionChange}
-                />
+        {/** textbox for contest description */}
+        <label htmlFor="description">Description:</label>
+        <textarea
+          id="description"
+          name="description"
+          value={description}
+          onChange={handleDescriptionChange}
+        />
 
-                <label htmlFor="max_score">Max Score:</label>
-                <input
-                    type="number"
-                    id="max_score"
-                    name="max_score"
-                    value={max_score}
-                    onChange={handleMaxScoreChange}
-                />
+        {/** input field for contest max score */}
+        <label htmlFor="max_score">Max Score:</label>
+        <input
+          type="number"
+          id="max_score"
+          name="max_score"
+          value={max_score}
+          onChange={handleMaxScoreChange}
+        />
 
-                <button type="submit" disabled={!canSubmit}>Save Contest</button>
-            </form>
-        </section>
-    );
+        {/** submit button */}
+        <button type="submit" disabled={!canSubmit}>Save Contest</button>
+      </form>
+    </section>
+  );
 
-    return (
-        <div>
-            {content}
-        </div>
-    )
+  // render
+  return (
+    <div>
+        {content}
+    </div>
+  )
 }
 
-export default NewContest
+export default NewContest;
