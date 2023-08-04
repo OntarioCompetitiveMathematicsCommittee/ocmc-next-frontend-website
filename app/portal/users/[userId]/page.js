6f0df10daeecd3add588cf42192de956323ed0dc
yanziguoo <1guoyan@hdsb.ca>
2023-08-04 15:13:25 -0400

    comment edit user page

--- app/portal/users/[userId]/page.js
+++ app/portal/users/[userId]/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+// import required modules and components
 import { useParams } from 'next/navigation'
 import { useSelector } from 'react-redux'
 import { selectUserById } from '@components/features/users/usersApiSlice'
@@ -7,10 +8,13 @@ import { selectUserById } from '@components/features/users/usersApiSlice'
 import EditUserForm from '@components/features/users/EditUserForm'
 
 const EditUser = () => {
+    // get user id
     const params = useParams();
     const id = params.userId;
     
+    // get user data using id
     const user = useSelector((state) => selectUserById(state, id));
+    // render component if user data is available, otherwise display loading message
     const content = user ? <EditUserForm user={user} editingAll={true}/> : <div>Loading...</div>
     return content;
 }
