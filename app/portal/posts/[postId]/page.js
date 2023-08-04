77a307863756658d5bc726331a6ce1b98a7b20fc
yanziguoo <1guoyan@hdsb.ca>
2023-08-03 20:03:57 -0400

    comment edit post page

--- app/portal/posts/[postId]/page.js
+++ app/portal/posts/[postId]/page.js
@@ -1,16 +1,20 @@
 "use client";
 
+// import required modules and components
 import { useParams } from 'next/navigation';
 import { useSelector } from 'react-redux';
 import { selectPostById } from '@components/features/posts/postsApiSlice';
 import EditPostForm from '@components/features/posts/EditPostForm';
 
 const EditPost = () => {
+    // get post id
     const params = useParams();
     const id = params.postId;
 
+    // select post data using post id
     const post = useSelector(state => selectPostById(state, id));
 
+    // render EditPostForm component if data is available, otherwise display loading message
     const content = post ? <EditPostForm post={post} id={id} /> : <p>Loading...</p>
     return content;
 }
