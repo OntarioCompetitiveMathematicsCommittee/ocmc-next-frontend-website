730e3a943498c0b13ea51420b8f0e1131f5d3d25
yanziguoo <1guoyan@hdsb.ca>
2023-08-04 12:24:10 -0400

    comment proctors loading page

--- app/portal/proctors/loading.js
+++ app/portal/proctors/loading.js
@@ -1,6 +1,8 @@
+// import required modules and components
 import React from 'react'
 
 const Loading = () => {
+  // display loading page
   return (
     <div>Loading..........................</div>
   )
