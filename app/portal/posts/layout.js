b78af76d391ba2f67df3a36b2667a59e93a8aa0d
yanziguoo <1guoyan@hdsb.ca>
2023-08-03 20:23:19 -0400

    comment posts layout file

--- app/portal/posts/layout.js
+++ app/portal/posts/layout.js
@@ -1,8 +1,10 @@
+// import required modules and components
 import RequireAuth from '@components/RequireAuth'
 import { ROLES } from '@config/roles'
 
 const PostsLayout = ({ children }) => {
     return (
+        // restrict access to users with admin or executive roles
         <RequireAuth allowedRoles={[ROLES.Admin, ROLES.Executive]}>
             {children}
         </RequireAuth>
