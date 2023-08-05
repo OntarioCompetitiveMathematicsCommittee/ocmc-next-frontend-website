075222d3f98ab28a55c8a3b1d63f147b2e1cd22c
yanziguoo <1guoyan@hdsb.ca>
2023-08-04 21:05:31 -0400

    comment posts page

--- app/portal/posts/page.js
+++ app/portal/posts/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+// import required modules and components
 import { useState } from 'react'
 import { useGetPostsQuery } from '@components/features/posts/postsApiSlice'
 import Link from 'next/link'
@@ -8,36 +9,45 @@ import Post from '@components/features/posts/Post'
 import TableHead from '@components/TableHead'
 
 const PostsList = () => {
+  // fetch list of posts
   const { data: posts, isLoading, isSuccess, isError, error } = useGetPostsQuery(undefined, {
     pollingInterval: 60000,
     refetchOnFocus: true,
     refetchOnMountOrArgChange: true
   })
 
+  // state of search query input
   const [searchQuery, setSearchQuery] = useState("");
 
 
   let content;
 
+  // page loading
   if (isLoading) content = <p>Loading...</p>;
 
+  // display error message
   if (isError) content = <p>{error.error}</p>;
 
+  // render list of posts
   if (isSuccess) {
     const { ids } = posts;
 
+    // list of posts based on ids
     const tableContent = ids?.length
       ? ids.map((postId) => <Post key={postId} postId={postId} searchQuery={searchQuery}/>) : null;
 
     content = (
       <div className='w-full h-full flex flex-col py-24 items-center gap-24 overflow-scroll'>
 				<div className='text-center flex flex-col gap-2 items-center'>
+          {/** title */}
           <h1 className={"text-5xl font-bold"}>Posts List</h1>
+          {/** button linked to create new post page */}
           <Link className='w-64 py-2 px-2 rounded-md bg-brandBlue-500 text-white flex justify-center' href="/portal/posts/new">
             <button>Create New Post</button>
           </Link>
         </div>
 
+        {/** search query input field */}
         <div className='flex flex-col w-4/5 gap-4'>
 					<input
 						className="w-64 py-2 px-2 rounded-md border-2 "
@@ -46,7 +56,8 @@ const PostsList = () => {
 						onChange={e => setSearchQuery(e.target.value)}
 						placeholder="Search Posts..."
 					/>
-        
+
+          {/** table to display list of posts */}
           <table>
             <TableHead headings={["Title", "Content", "Description", "Visible", "Date Posted", "Edit"]}/>
             <tbody>{tableContent}</tbody>
