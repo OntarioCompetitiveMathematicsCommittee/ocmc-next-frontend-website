878d006750db1cdc7b04c06c3ea80d0007588369
yanziguoo <1guoyan@hdsb.ca>
2023-08-04 21:08:57 -0400

    comment proctor school-users page

--- app/portal/proctors/school-users/page.js
+++ app/portal/proctors/school-users/page.js
@@ -1,5 +1,6 @@
 "use client"
 
+// import required modules and components
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useGetUsersQuery, selectUserById } from '@components/features/users/usersApiSlice'
@@ -9,35 +10,44 @@ import useAuth from '@components/hooks/useAuth'
 import TableHead from '@components/TableHead'
 
 const ProctorUsersList = () => {
+	// fetch list of users/participants
 	const { data: users, isLoading, isSuccess, isError, error } = useGetUsersQuery(undefined, {
 		pollingInterval: 60000,
 		refetchOnFocus: true,
 		refetchOnMountOrArgChange: true,
 	});
 
+	// get proctor id and user data
 	const proctor_id = useAuth().id;
 	const proctor = useSelector((state) => selectUserById(state, proctor_id));
 
+	// state of search query input
 	const [searchQuery, setSearchQuery] = useState('')
 
 	let content;
 
+	// page loading
 	if (isLoading) content = <p>Loading...</p>
 
+	// display error
 	if (isError) content = <p>{error.error}</p>
 
+	// render list of participants
 	if (isSuccess) {
 		const { ids } = users;
 
+		// list of participants based on their ids
 		const tableContent = ids?.map(userId => <ProctorUserDisplay key={userId} userId={userId} school={proctor.school} searchQuery={searchQuery} />);
 	
 		content = (
 			<div className='w-full h-full flex flex-col py-24 items-center gap-24 overflow-scroll'>
+				{/** title with proctor school name */}
 				<div className='text-center'>
 					<h1 className={"text-5xl"}>Registered Participants for</h1>
 					<h2 className={"text-5xl font-bold text-brandBlue-900"}>{proctor.school}</h2>
 				</div>
 				<div className='flex flex-col w-4/5 gap-4'>
+					{/** search query input field */}
 					<input
 						className="w-64 py-2 px-2 rounded-md border-2 "
 						type="text"
@@ -45,6 +55,7 @@ const ProctorUsersList = () => {
 						onChange={e => setSearchQuery(e.target.value)}
 						placeholder="Search Students..."
 					/>
+					{/** table to display list of registered participants under the proctor */}
 					<table className='table-auto border-spacing-10'>
 						<TableHead headings={["Username", "First Name", "Last Name", "Email", "Reset Password"]}/>
 						<tbody className='text-xl'>{tableContent}</tbody>
