d2f1dd3ad5c467e32a92ce0aadf006244f590fb9
m-shao <s201083816@ddsbstudent.ca>
2023-08-09 14:10:32 -0400

    minor changes, title page styled for portal

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
 
     return (
         <>
-            <nav className='flex justify-center w-full bg-brandNeutral-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 
+            <nav className='flex justify-center w-full bg-brandNeutral-200 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 
                 py-2 px-4 fixed top-0 left-0 z-50 shadow-sm'>
                 <div className='max-w-[90rem] w-full flex justify-between items-center'>
                     <Link className='flex items-center text-2xl font-medium gap-2' href='/'>
