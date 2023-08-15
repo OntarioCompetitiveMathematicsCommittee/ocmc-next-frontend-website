fa6b6c3ab60260a77cc2c70c61566ec2eaed617b
Minglun Shao <dev.minglunshao@gmail.com>
2023-08-15 16:29:28 -0400

    Revert "Merge pull request #14 from OntarioCompetitiveMathematicsCommittee/commenting"
    This reverts commit bebdad853b00355f5c3ccd4eb985de68eecbca7d, reversing
    changes made to f2beac5b3c9c6cf82025eaa2b44f485e559155f9.

--- app/portal/page.js
+++ app/portal/page.js
@@ -1,19 +1,17 @@
 "use client"
 
-// import required modules and components
 import useAuth from '@components/hooks/useAuth'
 
 const Portal = () => {
-    // get user data
     const { first_name, last_name, highest_status } = useAuth();
     return (
         <>
             <div className='w-full h-full flex flex-col justify-center pb-48 items-center gap-16 overflow-scroll'>
-                {/** title with proctor school name */}
-                <div className='text-center'>
-                  <h1 className={"text-3xl lg:text-7xl md:text-5xl"}>Welcome To</h1>
-                  <h2 className={"text-3xl lg:text-7xl md:text-5xl font-bold text-brandBlue-900"}>The OCMC Student Portal</h2>
-                </div>
+				{/** title with proctor school name */}
+				<div className='text-center'>
+					<h1 className={"text-3xl lg:text-7xl md:text-5xl"}>Welcome To</h1>
+					<h2 className={"text-3xl lg:text-7xl md:text-5xl font-bold text-brandBlue-900"}>The OCMC Student Portal</h2>
+				</div>
                 <h2 className={"text-xl lg:text-5xl md:text-3xl text-brandBlue-900"}>{highest_status} Portal</h2>
             </div>
         </>
