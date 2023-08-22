37b1bbacaada32fde0d7d4c45da1da84229b3ebd
Minglun Shao <dev.minglunshao@gmail.com>
2023-08-22 19:59:00 -0400

    can select contests for contest viewing

--- app/portal/proctors/contests/page.js
+++ app/portal/proctors/contests/page.js
@@ -1,72 +1,56 @@
 "use client"
 
-import { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-
-import { useGetUsersByContestMutation, selectUserById } from '@components/features/users/usersApiSlice'
-import useAuth from '@components/hooks/useAuth'
+// import required modules and components
+import { useGetContestsQuery } from '@components/features/contests/contestsApiSlice'
+import ViewContest from '@components/features/contests/ViewContest'
+import { useState } from 'react'
 
 import TableHead from '@components/TableHead'
 
-const Contests = () => {
-    const [yoyoyo, {isLoading, isSuccess, isError, error}] = useGetUsersByContestMutation();
-    const [users, setUsers] = useState([]);
+const ContestsList = () => {
 
-    const proctor_id = useAuth().id;
-	const proctor = useSelector((state) => selectUserById(state, proctor_id));
+	// fetch list of contests
+	const { data: contests, isLoading, isSuccess, isError, error } = useGetContestsQuery(undefined, {
+		pollingInterval: 60000,
+		refetchOnFocus: true,
+		refetchOnMountOrArgChange: true
+	})
 
-    const fetchData = async () => {
-        const users = await yoyoyo({contest_id: "6494e8d8383c89a925ffdc9a", school: proctor.school});
-        setUsers(users);
-    }
-
-    useEffect(() => {
-        if (proctor){
-            fetchData()
-        }
-    }, [proctor, yoyoyo])
-
-    let content;
-    let userScore
+	let content;
 
 	// page loading
-	if (isLoading) content = <p>Loading...</p>
+	if (isLoading) content = <p>Loading...</p>;
 
-	// display error
-	if (isError) content = <p>{error.error}</p>
+	// display error message
+	if (isError) content = <p>{error.error}</p>;
 
-	// render list of contest participants
+	// render list of contests
 	if (isSuccess) {
-	
+		const { ids } = contests;
+
+		// list of contests based on their ids
+		const tableContent = ids?.length
+			? ids.map((contestId) => <ViewContest key={contestId} contestId={contestId} />) : null;
+
 		content = (
 			<div className='flex flex-col items-center w-full h-full gap-24 py-24 overflow-scroll'>
-				<div className='text-center'>
-					<h1 className={"text-5xl"}>{"CONTEST"} Score</h1>
-                    <h2 className={"portalh2 text-brandBlue-900"}>{proctor?.school}</h2>
+				<div className='flex flex-col items-center gap-2 text-center'>
+					{/** title */}
+					<h1 className="portalh2">Contest List</h1>
 				</div>
+
 				<div className='flex flex-col w-4/5 gap-4'>
-					{/** table to display list of registered participants under the proctor */}
-					<table className='table-auto border-spacing-10'>
-						<TableHead headings={["Username", "First Name", "Last Name", "Email", "Score"]}/>
-						<tbody className='text-xl'>
-                            {users?.data?.map((user, index) => {
-                                userScore = user.contest_data.find(item => item.contest_id === "6494e8d8383c89a925ffdc9a").score;
-                                return (
-                                <tr className="bg-white border-2" key={index}>
-                                    <td className="p-4">{user.username}</td>
-                                    <td>{user.first_name}</td>
-                                    <td>{user.last_name}</td>
-                                    <td>{user.email}</td>
-                                    <td>{userScore === -1 ? "TBA" : userScore}</td>
-                                </tr>
-                                )})}
-                        </tbody>
+					{/** table to display list of contests */}
+					<table>
+						<TableHead headings={["Name", "Year", "Description", "Max Score", "Signups Active", "# of Signups", "View"]}/>
+						<tbody>{tableContent}</tbody>
 					</table>
 				</div>
 			</div>
 		);
-    }
-    return content;
+	}
+
+	return content;
 }
 
-export default Contests
+export default ContestsList
--- components/features/contests/ViewContest.jsx
+++ components/features/contests/ViewContest.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { useRouter } from 'next/navigation';
 import { useSelector } from "react-redux";
 import { selectContestById } from "./contestsApiSlice";
@@ -8,7 +9,6 @@ const Contest = ({ contestId }) => {
     const router = useRouter();
 
     if (contest) {
-
         const created = new Date(contest.createdAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
         const updated = new Date(contest.updatedAt).toLocaleString('en-US', { day: 'numeric', month: 'long', year: 'numeric' });
 
@@ -22,10 +22,8 @@ const Contest = ({ contestId }) => {
                 <td>{contest.max_score}</td>
                 <td>{contest.signups_active ? "Yes" : "No"}</td>
                 <td>{contest.signup_ids.length}</td>
-                {/* <td>{created}</td>
-                <td>{updated}</td> */}
                 <td>
-                    <button onClick={handleEdit}>Edit</button>
+                    <Link href={"/portal/proctors/contests/" + contestId}>View</Link>
                 </td>
             </tr>
         )
