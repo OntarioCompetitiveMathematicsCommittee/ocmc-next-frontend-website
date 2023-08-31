c9b354ec664c2af76afc16ad37be0fef6ceac234
Minglun Shao <dev.minglunshao@gmail.com>
2023-08-30 20:57:15 -0400

    parsing of csv fixed, more execs and directors added

--- app/portal/contests/input/[contestId]/page.js
+++ app/portal/contests/input/[contestId]/page.js
@@ -9,6 +9,7 @@ import BackButton from '@components/elements/BackButton';
 const EditContest = () => {
 	const [csv, setCsv] = useState('');
 	const [updates, setUpdates] = useState({});
+	const [errors, setErrors] = useState({});
 
 	const handleCsvChange = (e) => {
 		setCsv(e.target.value);
@@ -25,15 +26,23 @@ const EditContest = () => {
 		const csvArray = csv.split(',');
 		let updateObj = {};
 		for (let i = 0; i < Math.floor(csvArray.length / 2); i++) {
-			updateObj = {};
-			updateObj[csvArray[2 * i]] = csvArray[2 * i + 1];
-			setUpdates({...updates, ...updateObj});
+			//check if score is empty
+			if (csvArray[2 * i + 1] == ''){
+				delete updateObj[csvArray[2 * i]];
+			//check if it's a number
+			} else if (!/^\d+$/.test(csvArray[2 * i + 1])){
+				setErrors({...errors, [csvArray[2 * i]]: 'Score must be a number'})
+			} else{
+				updateObj[csvArray[2 * i]] = csvArray[2 * i + 1];
+			}
 		}
+		setUpdates(updateObj);
 		
 	}, [csv]);
 	
 	useEffect(() => {
 		console.log(updates);
+		console.log(errors)
 	}, [updates]);
 	
 
@@ -41,10 +50,10 @@ const EditContest = () => {
 	const content = (
 		<div className='flex flex-col items-center justify-center w-full h-full gap-8 pb-32'>
 			<BackButton path="/portal/contests"/>
-			<h1 className="portalh2">Edit Contest</h1>
+			<h1 className="portalh2">Input Scores</h1>
 			<form className='max-w-[90%] w-[60rem]'>
 				<div className='flex flex-col gap-2'>
-					<label className="text-xl text-brandBlue-900" htmlFor="description">Description:</label>
+					<label className="text-xl text-brandBlue-900" htmlFor="description">CSV (username1,score1,username2,score2):</label>
 					<textarea
 						className="w-full h-64 border-2"
 						id="description"
