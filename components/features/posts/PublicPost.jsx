c8847a7a03b4515ac383bbb6533f39191967ecdb
Minglun Shao <dev.minglunshao@gmail.com>
2023-09-08 01:11:09 -0400

    update

--- components/features/posts/PublicPost.jsx
+++ components/features/posts/PublicPost.jsx
@@ -37,7 +37,7 @@ const Post = ({ postId, searchQuery }) => {
             <div className='flex flex-col justify-start w-full gap-2 p-4 pb-6 bg-white border-2'>
                 <div className='flex gap-2'>
                     <h3 className='text-md'>{author}</h3>
-                    <p className='text-gray-500 text-md'>Last Edited: {created}</p>
+                    <p className='text-gray-500 text-md'>Last Edited: {updated}</p>
                 </div>
                 <button className='flex flex-col gap-2' onClick={() => {setExpanded(prev => !prev)}}>
                     <h1 className='text-2xl font-bold text-left font-[Montserrat] max-w-2xl'>{post.title}</h1>
