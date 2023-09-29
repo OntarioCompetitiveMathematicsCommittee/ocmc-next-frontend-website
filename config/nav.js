c6409bc578ac5f6482eb7c91470ee69dd6f0a6e7
yanziguoo <1guoyan@hdsb.ca>
2023-09-29 09:46:28 -0400

    revert??

--- config/nav.js
+++ config/nav.js
@@ -2,7 +2,6 @@ export const navLinks = {
     '/': 'Home',
     '/events': 'Events',
     '/math-contests': 'Contests',
-    // '/past-contests': 'Past Contests',
     '/posts': 'Announcements',
     '/contact': 'Contact Us',
 }
