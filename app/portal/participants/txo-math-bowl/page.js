10132138a0f6a4f324c5cdd13522295e0fca5a09
yanziguoo <1guoyan@hdsb.ca>
2024-07-03 13:39:01 -0700

    timezones

--- app/portal/participants/txo-math-bowl/page.js
+++ app/portal/participants/txo-math-bowl/page.js
@@ -1,14 +1,11 @@
 'use client';
 
 import React, { useState } from 'react';
-
 import Chess from '@/components/txo-math-bowl/games/Chess';
 import Sudoku from '@/components/txo-math-bowl/games/Sudoku';
 import Math from '@/components/txo-math-bowl/games/Math';
 import Trivia from '@/components/txo-math-bowl/games/Trivia';
-
 import GamesWrapper from '@/components/txo-math-bowl/GamesWrapper';
-
 import useAuth from '@hooks/useAuth';
 import { useSelector } from 'react-redux';
 import {
@@ -21,47 +18,44 @@ const TxOMathBowl = () => {
 	const { isSuccess, isLoading } = useGetUsersQuery(); // Fetch all users
 	const currUser = useSelector((state) => selectUserById(state, id));
 
-
-	
 	const games = [
 		{
 			component: <Chess key='chess' userId={id} />,
-			startDate: '2024-07-03T17:20',
-			endDate: '2024-07-03T17:45',
+			startDate: '2024-07-03T20:40:00Z', // UTC time for 12:30 PM EST
+			endDate: '2024-07-03T20:50:00Z', // UTC time for 12:35 PM EST
 		},
 		{
 			component: <Sudoku key='sudoku' userId={id} />,
-			startDate: '2024-07-03T14:35',
-			endDate: '2024-07-03T15:00',
+			startDate: '2024-07-02T20:30:00Z', // UTC time for 12:36 PM EST
+			endDate: '2024-07-02T20:40:00Z', // UTC time for 12:41 PM EST
 		},
 		{
 			component: <Math key='math' userId={id} />,
-			startDate: '2024-07-04T14:05',
-			endDate: '2024-07-04T14:25',
+			startDate: '2024-07-02T16:42:00Z', // UTC time for 12:42 PM EST
+			endDate: '2024-07-02T16:47:00Z', // UTC time for 12:47 PM EST
 		},
 		{
 			component: <Trivia key='trivia' userId={id} />,
-			startDate: '2024-07-04T17:05',
-			endDate: '2024-07-04T17:15',
+			startDate: '2024-07-02T16:48:00Z', // UTC time for 12:48 PM EST
+			endDate: '2024-07-02T16:53:00Z', // UTC time for 12:53 PM EST
 		},
 	];
+
+	if (isLoading) return <p>Loading...</p>;
 
 	const registered = currUser?.contest_data.find(
 		(contest) => contest.contest_id === '667fc6df604552c5babfb7fb'
 	);
 
-	if (isLoading) return <p>Loading...</p>;
-
 	if (isSuccess && registered) {
 		return (
-			<section className='flex flex-col items-center justify-start flex-1 w-full h-full'>
-				{games.map((game, index) => (
+			<section className='flex flex-col items-center justify-center flex-1 w-full h-full'>
+				{games.map((gameObj, index) => (
 					<GamesWrapper
 						key={index}
-						game={game.component}
-						startDate={game.startDate}
-						endDate={game.endDate}
-						maxTimePerQuestion={game.maxTimePerQuestion}
+						game={gameObj.component}
+						startDate={gameObj.startDate}
+						endDate={gameObj.endDate}
 					/>
 				))}
 			</section>
--- components/txo-math-bowl/GamesWrapper.jsx
+++ components/txo-math-bowl/GamesWrapper.jsx
@@ -1,28 +1,15 @@
 'use client';
 import { useEffect, useRef, useState } from 'react';
 
-const GamesWrapper = ({ game, startDate, endDate, maxTimePerQuestion }) => {
-	const startDateObject = useRef(new Date(startDate));
-	const endDateObject = useRef(new Date(endDate));
+const GamesWrapper = ({ game, startDate, endDate }) => {
 	const [currentDate, setCurrentDate] = useState(new Date());
 
 	const getTimeDifference = (startDate, endDate) => {
-		// Calculate the difference in milliseconds
-		let diff = endDate - startDate;
-
-		// Convert milliseconds to total seconds
-		let totalSeconds = Math.floor(diff / 1000);
-
-		// Calculate minutes and remaining seconds
-		let minutes = Math.floor(totalSeconds / 60);
-		let seconds = totalSeconds % 60;
-
-		// Format the time as "MM:SS"
-		let formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
-		if (formattedTime < '0:00') {
-			return 'Times up';
-		}
-		return formattedTime;
+		const diff = endDate - startDate;
+		const totalSeconds = Math.floor(diff / 1000);
+		const minutes = Math.floor(totalSeconds / 60);
+		const seconds = totalSeconds % 60;
+		const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 	};
 
 	useEffect(() => {
@@ -32,18 +19,17 @@ const GamesWrapper = ({ game, startDate, endDate, maxTimePerQuestion }) => {
 		return () => clearInterval(interval);
 	}, []);
 
-	const isWithinTimeFrame = currentDate >= startDateObject.current && currentDate <= endDateObject.current;
+	const startDateObject = new Date(startDate);
+	const endDateObject = new Date(endDate);
+
+	const isWithinTimeFrame = startDateObject <= currentDate && currentDate <= endDateObject;
 
 	return (
-		<div className='flex flex-col items-center justify-start w-full gap-8'>
-			{isWithinTimeFrame ? (
-				<>
-					<h1 className='text-3xl text-center'>
-						{getTimeDifference(currentDate, endDateObject.current)}
-					</h1>
-					{game}
-				</>
-			) : null}
+		<div className='flex flex-col items-center justify-center w-full h-full'>
+			<h1 className='text-3xl text-center'>
+				{getTimeDifference(currentDate, endDateObject)}
+			</h1>
+			{isWithinTimeFrame && game}
 		</div>
 	);
 };
