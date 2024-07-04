3571627abd6b3d0fcf41cfcd085d8aadf12f93aa
yanziguoo <1guoyan@hdsb.ca>
2024-07-03 21:54:27 -0400

    set times

--- app/portal/participants/txo-math-bowl/page.js
+++ app/portal/participants/txo-math-bowl/page.js
@@ -29,15 +29,15 @@ const TxOMathBowl = () => {
 		// 	startDate: '2024-07-04T00:05:00Z',
 		// 	endDate: '2024-07-04T00:30:00Z',
 		// },
-		// {
-		// 	component: <Math key='math' userId={id} />,
-		// 	startDate: '2024-07-04T01:15:00Z',
-		// 	endDate: '2024-07-04T01:30:00Z',
-		// },
+		{
+			component: <Math key='math' userId={id} />,
+			startDate: '2024-07-04T18:15:00Z',
+			endDate: '2024-07-04T18:20:00Z',
+		},
 		{
 			component: <Trivia key='trivia' userId={id} />,
-			startDate: '2024-07-04T01:40:00Z',
-			endDate: '2024-07-04T01:45:00Z',
+			startDate: '2024-07-04T21:05:00Z',
+			endDate: '2024-07-04T21:15:00Z',
 		},
 	];
 
