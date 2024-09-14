be6a5af1ec10f68c82db0a1808974d2b77a5e94b
SuperMK15 <smartiskush@gmail.com>
2024-09-13 21:13:43 -0400

    fixed contest registration form

--- components/features/contests/participants/ContestRegistrationForm.jsx
+++ components/features/contests/participants/ContestRegistrationForm.jsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { selectContestById, useUpdateContestSignupsMutation } from "../contestsApiSlice";
 import { useUpdateUserContestsMutation } from "@components/features/users/usersApiSlice";
 import { useSelector } from "react-redux";
@@ -15,8 +15,12 @@ const ContestRegistrationForm = ({ userId, contestId, username }) => {
     const [updateContestSignups, { isLoading, isSuccess, isError, error }] = useUpdateContestSignupsMutation();
     const [updateUserContests, { isLoading: isUserLoading, isSuccess: isUserSuccess, isError: isUserError, error: userError }] = useUpdateUserContestsMutation();
 
+    const isRequestInProgress = useRef(false);
+
     useEffect(() => {
-        if (isError && isUserError) alert(error.error + " " + userError.error);
+        if (isError && isUserError) {
+            alert(error.error + " " + userError.error);
+        }
     }, [isError, isUserError, error, userError]);
 
     useEffect(() => {
@@ -29,29 +33,42 @@ const ContestRegistrationForm = ({ userId, contestId, username }) => {
 
     const handleSignup = async (e) => {
         e.preventDefault();
-        if (isLoading || isUserLoading) return;
-        const type = isRegistered ? "remove" : "add";
-        await updateContestSignups({ id: contestId, participant_id: userId, type });
-        await updateUserContests({ username, contest_id: contestId, score:-1, type })
+        if (isLoading || isUserLoading || isRequestInProgress.current) return;
+
+        isRequestInProgress.current = true;
+
+        try {
+            const type = isRegistered ? "remove" : "add";
+            await updateContestSignups({ id: contestId, participant_id: userId, type });
+            await updateUserContests({ username, contest_id: contestId, score: -1, type });
+        } catch (error) {
+            // Handle any errors if necessary
+        } finally {
+            isRequestInProgress.current = false;
+        }
     }
 
     if (!signups_active) return null;
 
-    let registeredText = (isRegistered)
-        ? <p style={{'color': 'green'}}>You are registered for this contest!</p>
-        : <p style={{'color': 'red'}}>You are not registered for this contest.</p>;
-
-
     return (
-        <form className={"flex w-4/5 max-w-3xl px-12 py-6 rounded-xl justify-between items-start md:items-center shadow-sm flex-col md:flex-row gap-2 md:gap-0 " 
-            + (isRegistered ? "bg-emerald-100 border-2 border-emerald-200" : "bg-brandBlue-100 border-2 border-brandBlue-200")} onSubmit={handleSignup}>
+        <form
+            className={`flex w-4/5 max-w-3xl px-12 py-6 rounded-xl justify-between items-start md:items-center shadow-sm flex-col md:flex-row gap-2 md:gap-0 
+                ${isRegistered ? "bg-emerald-100 border-2 border-emerald-200" : "bg-brandBlue-100 border-2 border-brandBlue-200"}`}
+            onSubmit={handleSignup}
+        >
             <div className="flex flex-col">
-                <p className={"text-xl "  + (isRegistered ? "bg-emerald-100 text-emerald-700" : "bg-brandBlue-100 text-brandBlue-700")}>{year}</p>
+                <p className={`text-xl ${isRegistered ? "bg-emerald-100 text-emerald-700" : "bg-brandBlue-100 text-brandBlue-700"}`}>{year}</p>
                 <h2 className="text-3xl">{name}</h2>
                 <p>{description}</p>
             </div>
-            <button className={"w-auto px-8 h-full py-2 rounded-full text-white text-2xl " 
-                + (isRegistered ? "bg-emerald-800" : "bg-brandBlue-800")} type="submit" disabled={isLoading || isUserLoading}>{buttonText}</button>
+            <button
+                className={`w-auto px-8 h-full py-2 rounded-full text-white text-2xl 
+                    ${isRegistered ? "bg-emerald-800" : "bg-brandBlue-800"}`}
+                type="submit"
+                disabled={isLoading || isUserLoading}
+            >
+                {buttonText}
+            </button>
         </form>
     );
 }
