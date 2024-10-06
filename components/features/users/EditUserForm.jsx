69cb2071b253562659ac3b0dbf7eb1db5aa7c0e6
SuperMK15 <smartiskush@gmail.com>
2024-10-05 22:50:38 -0400

    fixed reroute bug

--- components/features/users/EditUserForm.jsx
+++ components/features/users/EditUserForm.jsx
@@ -135,7 +135,7 @@ const EditUserForm = ({ user, editingAll }) => {
 
         if (confirmDelete) {
             await deleteUser({ id: user.id });
-            router.replace(submitRouter);
+            router.replace(submitRoute);
         }
     }
 
