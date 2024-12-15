fb2bc94d2ef555c239d6f22a7751460e13803147
SuperMK15 <smartiskush@gmail.com>
2024-12-14 21:50:01 -0500

    added download data button for proctor contest view

--- app/portal/proctors/contests/[contestId]/page.js
+++ app/portal/proctors/contests/[contestId]/page.js
@@ -35,6 +35,24 @@ const Contests = () => {
 		refetchOnMountOrArgChange: true
 	}) 
 
+	// function to download participants data
+	const handleDownloadParticipantsData = async (contest_id) => {
+		// generate csv file
+		let csvContent = "data:text/csv;charset=utf-8,";
+		csvContent += "Username,FirstName,LastName,Grade,Email,Score\n";
+		users.forEach((user) => {
+			const userScore = user.contest_data.find(item => item.contest_id === contest_id).score;
+			csvContent += `${user.username},${user.first_name},${user.last_name},${user.grade},${user.email},${userScore}\n`;
+		});
+		// create download link
+		const encodedUri = encodeURI(csvContent);
+		const link = document.createElement("a");
+		link.setAttribute("href", encodedUri);
+		link.setAttribute("download", `Participant Data ${contests.entities[contest_id].name}.csv`);
+		document.body.appendChild(link);
+		link.click();
+	}
+
     let content;
 
 	// display error
@@ -50,13 +68,19 @@ const Contests = () => {
                     <h2 className={"portalh2 text-brandBlue-900"}>{proctor?.school}</h2>
 				</div>
 				<div className='flex flex-col w-4/5 gap-4'>
+					<button
+						className="w-64 px-2 py-2 text-white bg-brandBlue-500 rounded-md"
+						onClick={() => handleDownloadParticipantsData(contest_id)}
+					>
+						Download Participant Data
+					</button>
 					{/** table to display list of registered participants under the proctor */}
 					<input
 						className="w-64 px-2 py-2 border-2 rounded-md "
 						type="text"
 						value={searchQuery}
 						onChange={e => setSearchQuery(e.target.value)}
-						placeholder="Search Contests..."
+						placeholder="Search Participants"
 					/>
 					<TableWrapper className='table-auto border-spacing-10'>
 						<TableHead headings={["Username", "Full Name", "Grade", "Email", "Score"]}/>
