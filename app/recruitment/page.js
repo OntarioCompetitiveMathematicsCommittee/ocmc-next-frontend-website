59dff40fdc2e9738ec54b8ae0069de2f1382bbb5
SuperMK15 <smartiskush@gmail.com>
2025-04-22 14:31:38 -0400

    added recruitment form

--- app/recruitment/page.js
+++ app/recruitment/page.js
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
 
 const RecruitmentPage = () => {
-  redirect('https://docs.google.com/forms/d/e/1FAIpQLSf3FWAHRShmuT9IS2zqynwTeOZaMO8pT10yx8VA4i6aBCM-ZQ/viewform?usp=sf_link');
+  redirect('https://docs.google.com/forms/d/e/1FAIpQLSear1C0-npQQY_MKMJ-PLJBeFM6OU6RtwAak7gOjONcl-FiLA/viewform');
   return null;
 }
 
